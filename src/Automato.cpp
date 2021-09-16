9bfd58afd78c04ba4dd317eb34f9e0f337994413
Ben Burdette <bburdette@gmail.com>
2021-09-16 13:52:30 -0600

    fix architecture, static ftn

--- src/Automato.cpp
+++ src/Automato.cpp
@@ -7,7 +7,7 @@ An Arduino library for the Automato sensor board
 #include <Automato.h>
 #include <Esp.h>
 
-static uint64_t Automato::macAddress() 
+uint64_t Automato::macAddress() 
 {
   return ESP.getEfuseMac();
 }
