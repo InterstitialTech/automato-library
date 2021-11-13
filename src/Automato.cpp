420afa8c303aa36895f5828ef3a1c92640510457
Ben Burdette <bburdette@gmail.com>
2021-11-13 11:12:23 -0700

    revise printMessage

--- src/Automato.cpp
+++ src/Automato.cpp
@@ -22,52 +22,27 @@ void setupMessage(message &m,
   int  length,
   int  payload) 
 {
-  memcpy((char*)m.frommac, (const char*)&frommac, 4);
-  memcpy((char*)m.tomac, (const char*)&tomac, 4);
+  memcpy((char*)m.frommac, (const char*)&frommac, 6);
+  memcpy((char*)m.tomac, (const char*)&tomac, 6);
   m.type = type;
   m.address = address;
   m.length = length;
   m.payload = payload;
 }
 
-void printMac(const char *cmac)
-{
-  Serial.print("mac0: ");
-  Serial.println((int)cmac[0]);
-  Serial.print("mac1: ");
-  Serial.println((int)cmac[1]);
-  Serial.print("mac2: ");
-  Serial.println((int)cmac[2]);
-  Serial.print("mac3: ");
-  Serial.println((int)cmac[3]);
-  Serial.print("mac4: ");
-  Serial.println((int)cmac[4]);
-  Serial.print("mac5: ");
-  Serial.println((int)cmac[5]);
-  Serial.print("mac6: ");
-  Serial.println((int)cmac[6]);
-  Serial.print("mac7: ");
-  Serial.println((int)cmac[7]);
-}
-
 void printMessage(message &m) 
 {
   Serial.println("message");
 
-  // Serial.print("frommac: ");
-  // printMac((char*)m.frommac);
+  Serial.print("frommac: ");
+  uint64_t mac = 0;
+  memcpy(((char*)&mac), &m.frommac, 6); 
+  Serial.println(mac);
 
-  Serial.println("frommac: ");
-  printMac((char*)&m.frommac);
-  // uint64_t mac = 0;
-  // memcpy(((char*)&mac), &m.frommac, 6); 
-  // Serial.println(mac);
-
-  Serial.println("tomac: ");
-  printMac((char*)&m.tomac);
-  // mac = 0;
-  // memcpy(((char*)&mac), &m.tomac, 6); 
-  // Serial.println(mac);
+  Serial.print("tomac: ");
+  mac = 0;
+  memcpy(((char*)&mac), &m.tomac, 6); 
+  Serial.println(mac);
 
   Serial.print("type: ");
   switch ((int)m.type) {
