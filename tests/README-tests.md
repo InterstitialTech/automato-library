de129810517264b48271290f3dd21037e15d0f08
Ben Burdette <bburdette@protonmail.com>
2022-03-31 16:51:02 -0600

    tests README; update build.sh; add shell.nix for nixos

---
+++ tests/README-tests.md
@@ -0,0 +1,10 @@
+To build these tests, first:
+
+```
+$ cd tests/
+$ git clone https://github.com/InterstitialTech/testduino.git
+```
+
+Then build the tests with `./build.sh`.   Then run the resulting executable, `./tests`.
+
+Assumes the gcc compiler is installed.
--- tests/build.sh
+++ tests/build.sh
@@ -1,2 +1,1 @@
-g++ -I ../src/ -I testduino  -o tests tests.cc ../src/AutomatoMsg.cpp ../src/AutomatoResult.cpp 
-# gcc -I ../src/ -o tests tests.cc ../src/Automato.cpp ../src/AutomatoMsg.cpp ../src/AutomatoResult.cpp -I
+g++ -I ../src/ -I testduino  -o tests tests.cc ../src/AutomatoMsg.cpp ../src/AutomatoResult.cpp testduino/Arduino.cpp testduino/Print.cpp
