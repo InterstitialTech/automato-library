e44d11bc6450b16d9f7ff327a1219ccd819d409a
Ben Burdette <bburdette@protonmail.com>
2022-06-05 13:31:29 -0600

    format code

--- tests/msgs.cc
+++ tests/msgs.cc
@@ -9,29 +9,29 @@ using namespace std;
 
 void readMsg(const char *fname)
 {
-  std::cout << std::endl << fname << std::endl;
-  Msgbuf mb;
+    std::cout << std::endl << fname << std::endl;
+    Msgbuf mb;
 
-  FILE * filp = fopen(fname, "rb");
-  int bytes_read = fread(mb.buf, sizeof(char), RH_RF95_MAX_MESSAGE_LEN, filp);
+    FILE * filp = fopen(fname, "rb");
+    int bytes_read = fread(mb.buf, sizeof(char), RH_RF95_MAX_MESSAGE_LEN, filp);
 
-  std::cout << "read bytes: " << bytes_read << std::endl;
+    std::cout << "read bytes: " << bytes_read << std::endl;
 
-  for (int i = 0; i < bytes_read; ++i)
-  {
-    std::cout << (int)mb.buf[i] << " " << endl;
-  }
+    for (int i = 0; i < bytes_read; ++i)
+    {
+        std::cout << (int)mb.buf[i] << " " << endl;
+    }
 
-  printPayload(mb.payload);
+    printPayload(mb.payload);
 }
 
 
 void writeMsg(Msgbuf &mb, const char * fname)
 {
-  size_t sz = payloadSize(mb.payload);
-  cout << fname << " sz: " << sz << endl;
-  FILE * filp = fopen(fname, "wb");
-  int bytes_read = fwrite(mb.buf, sizeof(char), sz, filp);
+    size_t sz = payloadSize(mb.payload);
+    cout << fname << " sz: " << sz << endl;
+    FILE * filp = fopen(fname, "wb");
+    int bytes_read = fwrite(mb.buf, sizeof(char), sz, filp);
 }
 
 void writeMsgs()
@@ -101,35 +101,35 @@ void writeMsgs()
 
 void readMsgs()
 {
-  readMsg("rustmsgs-out/ack.bin");
+    readMsg("rustmsgs-out/ack.bin");
 
 
 
-  readMsg("rustmsgs-out/fail.bin");
-  readMsg("rustmsgs-out/pinmode.bin");
-  readMsg("rustmsgs-out/readpin.bin");
-  readMsg("rustmsgs-out/readpinreply.bin");
-  readMsg("rustmsgs-out/writepin.bin");
-  readMsg("rustmsgs-out/readanalog.bin");
-  readMsg("rustmsgs-out/readanalogreply.bin");
-  readMsg("rustmsgs-out/readmem.bin");
-  readMsg("rustmsgs-out/readmemreply.bin");
-  readMsg("rustmsgs-out/writemem.bin");
-  readMsg("rustmsgs-out/readinfo.bin");
-  readMsg("rustmsgs-out/readinforeply.bin");
-  readMsg("rustmsgs-out/readhumidity.bin");
-  readMsg("rustmsgs-out/readhumidityreply.bin");
-  readMsg("rustmsgs-out/readtemperature.bin");
-  readMsg("rustmsgs-out/readtemperaturereply.bin");
-  readMsg("rustmsgs-out/readfield.bin");
-  readMsg("rustmsgs-out/readfieldreply.bin");
+    readMsg("rustmsgs-out/fail.bin");
+    readMsg("rustmsgs-out/pinmode.bin");
+    readMsg("rustmsgs-out/readpin.bin");
+    readMsg("rustmsgs-out/readpinreply.bin");
+    readMsg("rustmsgs-out/writepin.bin");
+    readMsg("rustmsgs-out/readanalog.bin");
+    readMsg("rustmsgs-out/readanalogreply.bin");
+    readMsg("rustmsgs-out/readmem.bin");
+    readMsg("rustmsgs-out/readmemreply.bin");
+    readMsg("rustmsgs-out/writemem.bin");
+    readMsg("rustmsgs-out/readinfo.bin");
+    readMsg("rustmsgs-out/readinforeply.bin");
+    readMsg("rustmsgs-out/readhumidity.bin");
+    readMsg("rustmsgs-out/readhumidityreply.bin");
+    readMsg("rustmsgs-out/readtemperature.bin");
+    readMsg("rustmsgs-out/readtemperaturereply.bin");
+    readMsg("rustmsgs-out/readfield.bin");
+    readMsg("rustmsgs-out/readfieldreply.bin");
 }
 
 
 void showSyntax() {
-  cout << "msgs syntax:" << endl
-    << "msgs in <dirname>" << endl
-    << "msgs out <dirname>" << endl;
+    cout << "msgs syntax:" << endl
+         << "msgs in <dirname>" << endl
+         << "msgs out <dirname>" << endl;
 }
 
 
@@ -139,22 +139,22 @@ int main(int argc, char *argv[])
     if (argc == 3)
     {
         if (strcmp(argv[1], "in") == 0) {
-         readMsgs(argv[2]);
-  return 0;
+            readMsgs(argv[2]);
+            return 0;
         }
         else if (strcmp(argv[1], "out") == 0) {
-         writeMsgs(argv[2]);
-  return 0;
+            writeMsgs(argv[2]);
+            return 0;
         }
         else
         {
-          showSyntax();
-  return 1;
+            showSyntax();
+            return 1;
         }
     }
     else
-{
-          showSyntax();
-  return 1;
-        }
+    {
+        showSyntax();
+        return 1;
+    }
 }
